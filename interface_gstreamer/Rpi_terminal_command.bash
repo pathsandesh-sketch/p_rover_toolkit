ce04b18ed7ba663de9214632050eb94a545d1712
pathsandesh-sketch <pathsandesh@gmail.com>
2026-05-21 13:05:14 +0530

    Update Rpi_terminal_command.bash

--- interface_gstreamer/Rpi_terminal_command.bash
+++ interface_gstreamer/Rpi_terminal_command.bash
@@ -1,2 +1,1 @@
-
-gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! video/x-raw,width=640,height=480,framerate=30/1 ! x264enc tune=zerolatency bitrate=1500 speed-preset=ultrafast ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host=10.2.131.184 port=8282
+gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! video/x-raw,width=640,height=480,framerate=30/1 ! x264enc tune=zerolatency bitrate=1500 speed-preset=ultrafast ! h264parse ! mpegtsmux ! tcpclientsink host=10.2.131.184 port=8282
